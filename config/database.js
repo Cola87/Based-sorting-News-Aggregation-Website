b5e3d0988c5a8a1fde6e379019a1701bf72f2127
Jackie-code <842052794@qq.com>
2018-07-29 07:41:37 +0800

    first commit

---
+++ config/database.js
@@ -0,0 +1,13 @@
+/**
+ * Created by liuxu on 2018/7/3.
+ */
+
+module.exports = {
+    mysql :{
+        host : 'localhost',
+        user : 'root',
+        password : 'zekaijiang',
+        database : 'NEWSWEB',
+        port : 3306
+    }
+}
